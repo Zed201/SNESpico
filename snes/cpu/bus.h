c70335179defada69d4981859491b75adc80ac66
Luiz Gustavo <lgpss@cin.ufpe.br>
2024-11-28 16:47:24 -0300

    Memoring

--- snes/cpu/bus.h
+++ snes/cpu/bus.h
@@ -4,9 +4,13 @@
 
 #define KB (1 << 10)
 #define RAM_MAX_MEMORY 128 * KB
-#define ROM_MAX_MEMORY 32 * KB // por enquanto temporario o tamanho
+#define ROM_MAX_MEMORY 600 * KB // por enquanto temporario o tamanho, so para teste
 
 #define BYTES_TO_WORD(byteH, byteL) (byteH << 8) | byteL
+
+// mascara para os 8 bits menos e menos significativos de 16 bits
+#define LOW_BYTES_MASK 0x00FF
+#define HIGH_BYTES_MASK 0xFF00
 
 class Bus
 {
@@ -14,14 +18,21 @@ private:
     uint8_t m_Memory[RAM_MAX_MEMORY];
     uint8_t rom_Memory[ROM_MAX_MEMORY];
 
-    // fazer genericos pois o codigo da ram e do rom para deslocamente vai ser basicamente o mesmo
 
-    uint8_t __Read_Byte(uint32_t addr, uint8_t mem);
-    uint16_t __Read_Word(uint32_t addr, uint8_t mem);
-    void __Set_Byte(uint32_t addr, uint8_t byte, uint8_t mem);
-    void __Set_Word(uint32_t addr, uint16_t word, uint8_t mem);
+    // fazer genericos pois o codigo da ram e do rom para deslocamente vai ser basicamente o mesmo
+    uint8_t __Read_Byte(uint32_t addr, uint8_t* mem);
+    uint16_t __Read_Word(uint32_t addr, uint8_t* mem);
+    void __Set_Byte(uint32_t addr, uint8_t byte, uint8_t* mem);
+    void __Set_Word(uint32_t addr, uint16_t word, uint8_t* mem);
+
+    /* 
+        TODO: Fazer lógica de paginas e dos bancos de memória,
+        pois a memória do rasberry pi pico é de 264kB e tem 2MB de flash
+     */
 
 public:
+    // so para teste
+    uint64_t rom_Size = 0;
     Bus();
     ~Bus();
 
@@ -34,14 +45,12 @@ public:
     void Ram_Reset();
 
     // TODO: identificar os tipos de rom, com base nos endereços
-    void ReadRom(char * file_name);
+    bool ReadRom(char * file_name);
 
     uint8_t Rom_ReadByte(uint32_t addr);
     uint16_t Rom_ReadWord(uint32_t addr);
 
     void Rom_SetByte(uint32_t addr, uint8_t byte);
     void Rom_SetWord(uint32_t addr, uint16_t word);
-
-
 
 };
